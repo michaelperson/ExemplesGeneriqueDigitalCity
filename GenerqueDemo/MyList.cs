f14db0458ea2ef3cb2df9a0ada2d0aff9f3be3d2
michael person <michael.person@cognitic.be>
2025-09-26 14:30:04 +0200

    Exemple générique au niveau de la classe

---
+++ GenerqueDemo/MyList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerqueDemo
+{
+    public class MyList<T> 
+    { 
+        ArrayList list;
+        public MyList()
+        {
+            list = new ArrayList(); 
+        }
+        public void Add(T item)  
+        {   
+                list.Add(item);            
+        }
+
+        public void Remove(T item)
+        {
+            if (list.Contains(item)) list.Remove(item);
+            else throw new InvalidOperationException("Item n'existe pas");
+        }
+
+        public T Get(int index)
+        {
+            if (index > list.Count - 1 || index < 0) throw new IndexOutOfRangeException();
+            return (T)list[index];
+        }        
+    }
+}
--- GenerqueDemo/Program.cs
+++ GenerqueDemo/Program.cs
@@ -4,15 +4,15 @@ using GenerqueDemo.Models;
 
 Console.WriteLine("Hello, World!");
 
-Personnage personnage = new Personnage();
-personnage.Name = "PtiteGoutte";
-personnage.FirstName = "Justine";
-personnage.BirthDate = DateTime.Now;
+//Personnage personnage = new Personnage();
+//personnage.Name = "PtiteGoutte";
+//personnage.FirstName = "Justine";
+//personnage.BirthDate = DateTime.Now;
 
-Meubles meubles = new Meubles();
-meubles.Marque = "Giocomo";
-meubles.Prix = 456985;
-meubles.EstAntiquite = true;
+//Meubles meubles = new Meubles();
+//meubles.Marque = "Giocomo";
+//meubles.Prix = 456985;
+//meubles.EstAntiquite = true;
 
 //Animal animal = new Animal();
 //animal.Name = "Bernie";
@@ -31,16 +31,29 @@ meubles.EstAntiquite = true;
 //UIManager.DisplayInfo<Personnage>(personnage);
 //UIManager.DisplayInfo<Meubles>(meubles);
 //UIManager.DisplayInfo<Animal>(animal);
-Dictionary<string, object> lesinfosPourLefou = new();
+//Dictionary<string, object> lesinfosPourLefou = new();
 
-lesinfosPourLefou.Add("Name", "Lefou");
-lesinfosPourLefou.Add("FirstName", "Du centre");
-lesinfosPourLefou.Add("BirthDate", new DateTime(1975, 3, 2));
+//lesinfosPourLefou.Add("Name", "Lefou");
+//lesinfosPourLefou.Add("FirstName", "Du centre");
+//lesinfosPourLefou.Add("BirthDate", new DateTime(1975, 3, 2));
 
-Personnage leFou = UIManager.Usine<Personnage>(lesinfosPourLefou);
- 
-Console.WriteLine(leFou);
+//Personnage leFou = UIManager.Usine<Personnage>(lesinfosPourLefou);
+
+//Console.WriteLine(leFou);
+
 //Je voudrais transmettre un dictionnaire clé valeur qui contient les infos de mon futur objet
+List<int> list = new List<int>();
+list.Add(4);
+list.Remove(6);
+
+
+MyList<int> listDentier = new MyList<int>();
+
+ 
+  // ça passe pas ok
+double valeur =(double)  listDentier.Get(0);
+Console.WriteLine(listDentier.Get(0));
+listDentier.Remove(4);
 
 // créer l'objet et le renvoyer
-
+Console.ReadKey();
